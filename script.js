6ffefd910a626896c253a9ae640909bd89a2ffc3
gem1901 <gemmatyrrell88@gmail.com>
2021-04-20 18:26:49 +0000

    add question arrays

--- script.js
+++ script.js
@@ -14,23 +14,169 @@ function updateScore () {
 
 }
 function timer () {
-    
+
 }
 function displayGeneralKnowledgeQ () {
+    let generalKnowledgeQuestions [
+        {'question': 'dummyq1', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq2', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq3', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq4', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq5', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq6', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq7', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq8', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq9', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq10','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq11','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq12','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq13','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq14','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq15','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq16','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq17','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq18','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq19','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq20','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+    ];
+    for (i=0, i,<=15, i++) {
+        
+    }
+
 
 }
 function displayLyricsPreQ () {
+    let lyricsPreQuestions [
+        {'question': 'dummyq1', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq2', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq3', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq4', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq5', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq6', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq7', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq8', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq9', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq10','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq11','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq12','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq13','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq14','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq15','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq16','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq17','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq18','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq19','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq20','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+
+    ]
 
 }
 function displayLyricsPostQ () {
+    let lyricsPostQuestions [
+        {'question': 'dummyq1', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq2', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq3', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq4', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq5', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq6', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq7', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq8', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq9', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq10','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq11','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq12','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq13','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq14','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq15','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq16','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq17','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq18','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq19','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq20','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+
+    ]
 
 }
 function displayArtist80sQ () {
+    let artist80sQuestions [
+        {'question': 'dummyq1', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq2', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq3', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq4', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq5', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq6', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq7', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq8', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq9', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq10','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq11','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq12','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq13','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq14','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq15','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq16','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq17','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq18','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq19','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq20','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+
+    ]
 
 }
 function displayArtist90sQ () {
+    let artist90sQuestions [
+        {'question': 'dummyq1', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq2', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq3', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq4', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq5', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq6', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq7', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq8', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq9', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq10','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq11','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq12','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq13','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq14','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq15','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq16','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq17','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq18','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq19','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq20','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+    ]
 
 }
 function displayArtistMoviesQ () {
+    let artistMoviesQuestions [
+        {'question': 'dummyq1', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq2', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq3', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq4', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq5', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq6', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq7', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq8', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq9', 'answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq10','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq11','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq12','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq13','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq14','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq15','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq16','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq17','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq18','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq19','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+        {'question': 'dummyq20','answer1':'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D'},
+
+    ]
 
 }
+
+
+
+
+
+
