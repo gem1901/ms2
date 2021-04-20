16c82621f2d9d30904b95297b9bf8d971adbae98
gem1901 <gemmatyrrell88@gmail.com>
2021-04-20 17:50:00 +0000

    initial js function structure

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+function runGame () {
 
+}
+function checkAnswer () {
+
+}
+function displayCorrect () {
+
+}
+function answer () {
+
+}
+function updateScore () {
+
+}
+function timer () {
+    
+}
+function displayGeneralKnowledgeQ () {
+
+}
+function displayLyricsPreQ () {
+
+}
+function displayLyricsPostQ () {
+
+}
+function displayArtist80sQ () {
+
+}
+function displayArtist90sQ () {
+
+}
+function displayArtistMoviesQ () {
+
+}
