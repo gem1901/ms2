ca9b3bcc93115e250f782c9b12382fd1b05bfc14
gem1901 <gemmatyrrell88@gmail.com>
2021-04-22 12:21:26 +0000

    add basic js function structure for game

--- script.js
+++ script.js
@@ -1,11 +1,62 @@
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
-},
+}
+,function gameChoices() {
+
+}
+,function runGeneralKnowGame() {
+
+}
+,function lyricsCategories() {
+
+}
+,function artistCategories() {
+
+}
+,function runPre90sGame() {
+
+}
+,function runPost90sGame() {
+
+}
+,function run80sGame() {
+
+}
+,function run90sGame() {
+
+}
+,function run00sGame() {
+
+}
+,function correctAnswer() {
+
+}
+,function wrongAnswer() {
+
+}
+,function nextQuestion() {
+
+}
+,function result() {
+
+}
+,function restartGame() {
+  
+}
 
 
+
+
+
+
+
+
+
+
+/* 
 function displayGeneralKnowledgeQ () {
     
-    /*
+    
     let question = generalKnowledgeQuestions[Math.floor(Math.random()*generalKnowledgeQuestions.length)];
     let score = 0;
     for (let i=0; i<=15;i++) {
