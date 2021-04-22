507334d920e70c94370398d63ca816b1466fa8b7
gem1901 <gemmatyrrell88@gmail.com>
2021-04-22 16:11:38 +0000

    start building functions

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+      button.addEventListener('click', function)
+
+    }
 }
 ,function gameChoices() {
 
@@ -7,10 +11,33 @@ document.addEventListener("DOMContentLoaded", function () {
 ,function runGeneralKnowGame() {
 
 }
+,document.getElementById("lyricsCategories").addEventListener("click", lyricsCategories)
 ,function lyricsCategories() {
+  document.getElementById(game-container).innerHTML=`
+  <div id="welcome-message">
+            <h2>Choose your category for _Missing Lyrics below to start quiz</h2><br>
+            <i class="far fa-lightbulb" id="lightbulb"></i>
+        </div>
+        <div id="answer-butons" class="btn-grid">
+                <h4>The quiz will begin once you select your category, good luck!</h4>
+                <button class="btn btn-lg" type="submit">Lyrics Pre 1990</button>
+                <button class="btn btn-lg" type="submit">Lyrics Post 1990</button>       
+         </div>`
 
 }
+,document.getElementById("artistCategories").addEventListener("click", artistCategories)
 ,function artistCategories() {
+  document.getElementById(game-container).innerHTML=`
+  <div id="welcome-message">
+            <h2>Choose your category for Name The Artist below to start quiz</h2><br>
+            <i class="far fa-lightbulb" id="lightbulb"></i>
+        </div>
+        <div id="answer-butons" class="btn-grid">
+                <h4>The quiz will begin once you select your category, good luck!</h4>
+                <button class="btn btn-lg" type="submit">80s</button>
+                <button class="btn btn-lg" type="submit">90s</button>
+                <button class="btn btn-lg" type="submit">00s</button>       
+         </div>`
 
 }
 ,function runPre90sGame() {
